af282b80895385ca7b766f4d7b0106e356698cd4
dimitur98 <dimitur.mihailov988@gmail.com>
2025-01-20 15:08:31 +0200

    Initial commit

---
+++ GiftPicker.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GiftPicker.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
